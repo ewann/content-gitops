97dce0267287d99fda78642e786aa9c876a3f887
Ewan <ewann@users.noreply.github.com>
2020-03-29 20:03:19 +0100

    oh

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Pandemic!"
+    return "Hello Pandemic via pr!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
